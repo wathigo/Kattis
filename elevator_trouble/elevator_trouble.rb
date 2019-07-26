357605419dcccd88cbed1b4843a7c16d54624801
wathigo <wathigosimon@gmail.com>
2019-07-26 04:07:10 -0400

    Implement elevator_trouble challenge

---
+++ elevator_trouble/elevator_trouble.rb
@@ -0,0 +1,20 @@
+def count_pushes(floors, start, goal, up, down)
+  remaining_floors = (floors.to_i - start.to_i) % up.to_i
+  if remaining_floors != 0
+    up_pushes = ((floors.to_i - start.to_i) / up.to_i) + 1
+    current_floors = remaining_floors + (up_pushes * up.to_i)
+    down_floors = current_floors - goal.to_i
+    return "use the stairs" if current_floors < goal.to_i || ((down_floors % down.to_i) != 0)
+    down_pushes = down_floors / down.to_i
+    return down_pushes + up_pushes
+  end
+  "use the stairs"
+end
+
+input_data = File.read("data/1.in").split
+output = count_pushes(input_data[0], input_data[1], input_data[2], input_data[3], input_data[4])
+puts output
+
+input_data = File.read("data/2.in").split
+output = count_pushes(input_data[0], input_data[1], input_data[2], input_data[3], input_data[4])
+puts output
